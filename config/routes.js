dab86c2424e296a10d7ca29f419e94752220bb73
Marious Davis <mlancelot88@gmail.com>
2018-10-26 11:13:49 -0500

    added Node Dependencies & User Registration. port specification

--- config/routes.js
+++ config/routes.js
@@ -1,17 +1,32 @@
-const axios = require('axios');
+// ---- Node Dependencies ----
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const db = require("../database/dbConfig.js");
+const { authenticate, generateToken } = require("./middlewares");
 
-const { authenticate } = require('./middlewares');
-
+// ---- API Endoints for Testing ----
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
+// ---- User Registration ----
 function register(req, res) {
   // implement user registration
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 14);
+  user.password = hash;
+  db("users")
+    .insert(user)
+    .then(ids => {
+      const id = ids[0];
+      return res.status(201).json({ newUserId: id });
+    })
+    .catch(err => res.status(500).json({ Error: "User registration failed" }));
 }
 
+// ---- User Login ----
 function login(req, res) {
   // implement user login
 }
@@ -19,12 +34,12 @@ function login(req, res) {
 function getJokes(req, res) {
   axios
     .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten"
     )
     .then(response => {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
